4104f849334ec4f7e753fe211b51388e5289d44d
armymp33 <Armymp33@gmail.com>
2026-01-07 00:52:33 -0600

    Fix routing for GitHub Pages base path

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/MajesticArt">
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/admin/login" element={<AdminLogin />} />
